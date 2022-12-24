6d7b9ffec48e96e32fbfb20f1fd62c3fc23a61c8
Diya <you@example.com>
2022-12-24 22:20:45 +0530

    create new table

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set("view engine","ejs");
 app.get("/", async(request,response) => {
     const allTodos = await Todo.getTodos();
     if(request.accepts("html")){
-        response.render('index', {
+        response.render('todos', {
             allTodos
         });
     }else{
